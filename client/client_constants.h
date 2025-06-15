697c65bb577c9375a278cc8eaa23714313b4799a
sebakrag <sebaskrag@gmail.com>
2025-06-14 23:06:05 -0300

    Agrego los spritesheets de players actualizados.

--- client/client_constants.h
+++ client/client_constants.h
@@ -14,15 +14,14 @@
 #define WEAPON_PATH std::string("client/assets/weapons")
 #define HUD_PATH std::string("client/assets/hud")
 
-#define PHOENIX_IMG (PLAYER_PATH + "/t_phoenix.bmp")
-#define L377_KREW_IMG (PLAYER_PATH + "/t_L377_krew.bmp")
-// #define ARTIC_AVENGER_IMG (PLAYER_PATH + "/t_artic_avenger.bmp")
-#define ARTIC_AVENGER_IMG (PLAYER_PATH + "/t_artic_avenger.png")
-#define GUERRILLA_IMG (PLAYER_PATH + "/t_guerrilla.bmp")
-#define SEAL_FORCE_IMG (PLAYER_PATH + "/ct_seal_force.bmp")
-#define GERMAN_GSG_9_IMG (PLAYER_PATH + "/ct_german_csg_9.bmp")
-#define UK_SAS_IMG (PLAYER_PATH + "/ct_uk_sas.bmp")
-#define FRENCH_GIGN_IMG (PLAYER_PATH + "/ct_french_gign.bmp")
+#define PHOENIX_IMG (PLAYER_PATH + "/t_phoenix_spritesheet.png")
+#define L377_KREW_IMG (PLAYER_PATH + "/t_L377_krew_spritesheet.png")
+#define ARTIC_AVENGER_IMG (PLAYER_PATH + "/t_artic_avenger_spritesheet.png")
+#define GUERRILLA_IMG (PLAYER_PATH + "/t_guerrilla_spritesheet.png")
+#define SEAL_FORCE_IMG (PLAYER_PATH + "/ct_seal_force_spritesheet.png")
+#define GERMAN_GSG_9_IMG (PLAYER_PATH + "/ct_german_csg_9_spritesheet.png")
+#define UK_SAS_IMG (PLAYER_PATH + "/ct_uk_sas_spritesheet.png")
+#define FRENCH_GIGN_IMG (PLAYER_PATH + "/ct_french_gign_spritesheet.png")
 
 // Weapons
 #define AK47_IMG (WEAPON_PATH + "/ak47_spritesheet.png")
